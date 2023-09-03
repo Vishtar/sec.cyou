31c5d17d2ccf6bfe63e61d748de2d7772c0cf12b
Vishtar <viktor.kishchuk@gmail.com>
2023-09-03 15:08:47 +0400

    Hydration another try

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
-const appRoot = document.getElementById('container')
+// const appRoot = document.getElementById('container')
 
-if (appRoot) {
-  const root = ReactDOM.createRoot(appRoot)
+// if (appRoot) {
+//   const root = ReactDOM.createRoot(appRoot)
 
-  root.render(<App />)
-}
+//   root.render(<App />)
+// }
 
-// ReactDOM.hydrateRoot(document.getElementById('container'), <App />);
+ReactDOM.hydrateRoot(document.getElementById('container'), <App />);
