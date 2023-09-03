3c58383663bd9dde4ec4e664d502bc3bc046f606
Vishtar <viktor.kishchuk@gmail.com>
2023-09-03 15:13:28 +0400

    Comment delete

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -3,12 +3,4 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
-// const appRoot = document.getElementById('container')
-
-// if (appRoot) {
-//   const root = ReactDOM.createRoot(appRoot)
-
-//   root.render(<App />)
-// }
-
 ReactDOM.hydrateRoot(document.getElementById('container'), <App />);
