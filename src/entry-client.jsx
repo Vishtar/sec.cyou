272f3f4112081a917408b32803bedf25d12c580d
Vishtar <viktor.kishchuk@gmail.com>
2023-09-03 15:01:50 +0400

    Disable hydration

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,4 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { App } from './App';
 
-ReactDOM.hydrateRoot(document.getElementById('container'), <App />);
+const appRoot = document.getElementById('container')
+
+if (appRoot) {
+  const root = ReactDOM.createRoot(appRoot)
+
+  root.render(<App />)
+}
+
+// ReactDOM.hydrateRoot(document.getElementById('container'), <App />);
